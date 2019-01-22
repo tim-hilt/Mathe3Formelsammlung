247f92bcc44fd7768bcb1b49293196abda9ed78d
Tim Hilt <timhilt@live.de>
2019-01-22 12:47:49 +0100

    Transformierte Rechteckfunktion hinzugefügt

--- auto/Mathe3Formelsammlung.el
+++ auto/Mathe3Formelsammlung.el
@@ -4,15 +4,23 @@
    (setq TeX-command-extra-options
          "--shell-escape")
    (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("scrreprt" "12pt" "a4paper")))
+                     '(("scrreprt" "12pt" "a4paper" "twoside")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("shorttoc" "tight")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "Title"
     "scrreprt"
     "scrreprt12"
     "shorttoc"
+    "scrlayer-scrpage"
     "pgfplots"
     "amssymb"
     "todonotes"
